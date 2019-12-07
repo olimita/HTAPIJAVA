fa934184569d5add0cfa0568fdf54c16b6c7746e
Juan Lozano <juclozanome@unal.edu.co>
2019-12-07 15:33:36 -0500

    Ready to commit 2.0

--- src/main/java/com/olima/ws/rest/model/DataBaseConnect.java
+++ src/main/java/com/olima/ws/rest/model/DataBaseConnect.java
@@ -8,12 +8,12 @@ public class DataBaseConnect {
 	
 	private Connection conexion;
 	
-	private String url = "jdbc:postgresql:ec2-174-129-254-216.compute-1.amazonaws.com:5432/de05vd2d92u17r";
+	private String url = "jdbc:postgresql://[ec2-50-19-95-77.compute-1.amazonaws.com]:5432/d2unleamt9c3kh";
 	
 	public void connectDB() throws SQLException {
 		try {
 			Class.forName("org.postgresql.Driver");
-			conexion = DriverManager.getConnection("jdbc:postgresql://[ec2-174-129-254-216.compute-1.amazonaws.com]:5432/de05vd2d92u17r", "rtnpyhcfabctic", "1e509d63689ae17ba84b736e8ce0eed58ed92a2ae20e7563ff58f380add78ee6");
+			conexion = DriverManager.getConnection(url, "kwcllsjyifqjvn", "8f7e5cbd461da9b9e0ade6ccc551dc06b2f75c17a5a1d921ef79d33cd7ef18a7");
 			System.out.println("Conectadito.");
 		} catch (ClassNotFoundException ex) {
 			// log an exception. for example:
